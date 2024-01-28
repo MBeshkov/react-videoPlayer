dcddbfed2b09b9a0e9b6296d6d1dc1d98da4488b
MBeshkov <u05mb19@abdn.ac.uk>
2024-01-28 19:30:12 +0000

    video list selections and styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,7 +13,7 @@ constructor(props){
 
     render (){
         return (
-        <div>
+        <div className="search-bar">
             <input 
             value={this.state.term}
             onChange={event => this.setState({ term: event.target.value})} />
